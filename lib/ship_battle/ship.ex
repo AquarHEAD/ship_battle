6a61513a1d63f61cde7753a038d9edf5e2ca32dc
AquarHEAD Lou <aquarhead@gmail.com>
2016-06-13 15:01:20 +0800

    Add shield damage calculation

--- lib/ship_battle/ship.ex
+++ lib/ship_battle/ship.ex
@@ -7,6 +7,11 @@ defmodule ShipBattle.Ship do
     GenServer.start_link(__MODULE__, id)
   end
 
+  def take_damage(id, dmg) do
+    target_pid = :gproc.lookup_local_name({:ship, id})
+    GenServer.cast(target_pid, {:take_damage, dmg})
+  end
+
   # GenServer Callbacks
 
   def init(id) do
@@ -16,6 +21,38 @@ defmodule ShipBattle.Ship do
     :gproc.add_local_name({:ship, id})
 
     # initialize state
-    {:ok, id}
+    shield = %{
+      hp: 450,
+      capacity: 450,
+      resist: %{
+        em: 0,
+        ther: 0.2,
+        kin: 0.4,
+        exp: 0.5
+      }
+    }
+
+    ship_state = %{
+      destroyed: false,
+      id: id,
+      shield: shield,
+    }
+
+    {:ok, ship_state}
+  end
+
+  # If ship is destroyed, then ignore any messages after
+  def handle_cast(_, %{destroyed: true} = state), do: {:noreply, state}
+
+  def handle_cast({:take_damage, dmg}, %{shield: shield} = state) do
+    total_dmg =
+      dmg
+      |> Enum.map(fn({dmg_type, dmg_value}) -> dmg_value * (1.0 - shield[:resist][dmg_type]) end)
+      |> Enum.sum
+
+    new_hp = max(shield[:hp] - total_dmg, 0)
+
+    new_shield = %{shield | hp: new_hp}
+    {:noreply, %{state | destroyed: new_hp == 0, shield: new_shield}}
   end
 end
